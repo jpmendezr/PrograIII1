7d04ef53054d26521005f130de568b5e6e4fe360
jpmendezr <62160794+jpmendezr@users.noreply.github.com>
2020-07-18 10:52:11 -0600

    Agendar

--- Proyecto.1Medico/Proyecto.1Medico/AgendarCita.vb
+++ Proyecto.1Medico/Proyecto.1Medico/AgendarCita.vb
@@ -1,3 +1,55 @@
 ﻿Public Class AgendarCita
 
+    Private Fecha As Date
+    Private Hora As Date
+    Private EspecialidadM As String
+    Private NombreM As String
+    Private NombreP As String
+
+    Public Property Fecha1 As Date
+        Get
+            Return Fecha
+        End Get
+        Set(value As Date)
+            Fecha = value
+        End Set
+    End Property
+
+    Public Property Hora1 As Date
+        Get
+            Return Hora
+        End Get
+        Set(value As Date)
+            Hora = value
+        End Set
+    End Property
+
+    Public Property EspecialidadM1 As String
+        Get
+            Return EspecialidadM
+        End Get
+        Set(value As String)
+            EspecialidadM = value
+        End Set
+    End Property
+
+    Public Property NombreM1 As String
+        Get
+            Return NombreM
+        End Get
+        Set(value As String)
+            NombreM = value
+        End Set
+    End Property
+
+    Public Property NombreP1 As String
+        Get
+            Return NombreP
+        End Get
+        Set(value As String)
+            NombreP = value
+        End Set
+    End Property
+
+
 End Class
