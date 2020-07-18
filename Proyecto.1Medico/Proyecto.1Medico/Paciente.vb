b0ab4305a2395ee7f5e3765dd49bc33575104dfd
MARIA HERRERA ZU�IGA <maherreraz@est.utn.ac.cr>
2020-07-18 10:52:36 -0600

    2comintpaciente

--- Proyecto.1Medico/Proyecto.1Medico/Paciente.vb
+++ Proyecto.1Medico/Proyecto.1Medico/Paciente.vb
@@ -1,3 +1,142 @@
 ﻿Public Class Paciente
+    Private cedula As String
+    Private NombreP As String
+    Private Apellidos As String
+    Private Peso As Double
+    Private Edad As Integer
+    Private Altura As Double
+    Private Enfermedades As String
+    Private TipoSangre As String
+    Private MedicamentosAlergicos As String
+    Private NombreFamiliar As String
+    Private TipoFamiliar As String
+    Private Telefono As Integer
+    Private Direccion As String
+    Private Medicamentos As String
 
+    Public Property pro_Cedula1 As String
+        Get
+            Return cedula
+        End Get
+        Set(value As String)
+            cedula = value
+        End Set
+    End Property
+
+    Public Property pro_NombreP1 As String
+        Get
+            Return NombreP
+        End Get
+        Set(value As String)
+            NombreP = value
+        End Set
+    End Property
+
+    Public Property pro_Apellidos1 As String
+        Get
+            Return Apellidos
+        End Get
+        Set(value As String)
+            Apellidos = value
+        End Set
+    End Property
+
+    Public Property pro_Peso1 As Double
+        Get
+            Return Peso
+        End Get
+        Set(value As Double)
+            Peso = value
+        End Set
+    End Property
+
+    Public Property pro_Edad1 As Integer
+        Get
+            Return Edad
+        End Get
+        Set(value As Integer)
+            Edad = value
+        End Set
+    End Property
+
+    Public Property pro_Altura1 As Double
+        Get
+            Return Altura
+        End Get
+        Set(value As Double)
+            Altura = value
+        End Set
+    End Property
+
+    Public Property pro_Enfermedades1 As String
+        Get
+            Return Enfermedades
+        End Get
+        Set(value As String)
+            Enfermedades = value
+        End Set
+    End Property
+
+    Public Property pro_TipoSangre1 As String
+        Get
+            Return TipoSangre
+        End Get
+        Set(value As String)
+            TipoSangre = value
+        End Set
+    End Property
+
+    Public Property pro_MedicamentosAlergicos1 As String
+        Get
+            Return MedicamentosAlergicos
+        End Get
+        Set(value As String)
+            MedicamentosAlergicos = value
+        End Set
+    End Property
+
+    Public Property pro_NombreFamiliar1 As String
+        Get
+            Return NombreFamiliar
+        End Get
+        Set(value As String)
+            NombreFamiliar = value
+        End Set
+    End Property
+
+    Public Property pro_TipoFamiliar1 As String
+        Get
+            Return TipoFamiliar
+        End Get
+        Set(value As String)
+            TipoFamiliar = value
+        End Set
+    End Property
+
+    Public Property pro_Telefono1 As Integer
+        Get
+            Return Telefono
+        End Get
+        Set(value As Integer)
+            Telefono = value
+        End Set
+    End Property
+
+    Public Property pro_Direccion1 As String
+        Get
+            Return Direccion
+        End Get
+        Set(value As String)
+            Direccion = value
+        End Set
+    End Property
+
+    Public Property pro_Medicamentos1 As String
+        Get
+            Return Medicamentos
+        End Get
+        Set(value As String)
+            Medicamentos = value
+        End Set
+    End Property
 End Class
