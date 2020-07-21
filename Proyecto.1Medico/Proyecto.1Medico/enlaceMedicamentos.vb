6ef2cb43f40a24d147a564d7f070233f5d4cecae
jpmendezr <62160794+jpmendezr@users.noreply.github.com>
2020-07-21 10:02:12 -0600

    claseMedicamento

---
+++ Proyecto.1Medico/Proyecto.1Medico/enlaceMedicamentos.vb
@@ -0,0 +1,32 @@
+﻿Public Class enlaceMedicamentos
+
+    'No existe una clase para enlazar los medicamentos.
+    'Por ello el atributo medicamentoRecetado de la clase ClsConsultaMedida debe ser un arreglo por agregación
+
+    Private MRecetado As String
+    Private enlaceMedicamento As List(Of String)
+
+    Public Property MRecetado1 As String
+        Get
+            Return MRecetado
+        End Get
+        Set(value As String)
+            MRecetado = value
+        End Set
+    End Property
+
+    Public Property EnlaceMedicamento1 As List(Of String)
+        Get
+            Return enlaceMedicamento
+        End Get
+        Set(value As List(Of String))
+            enlaceMedicamento = value
+        End Set
+    End Property
+
+    Public Sub AgregarM()
+        For Each Item In EnlaceMedicamento1
+            Me.EnlaceMedicamento1.Add(MRecetado1)
+        Next
+    End Sub
+End Class
