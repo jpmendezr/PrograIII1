5e6fb06168aa438bb61f4c8af8cac2923c42b154
MARIA HERRERA ZU�IGA <maherreraz@est.utn.ac.cr>
2020-07-21 18:54:52 -0600

    comint Consultorio Form

---
+++ Proyecto.1Medico/Proyecto.1Medico/FormConsultorio.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormConsultorio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtpresion = New System.Windows.Forms.TextBox()
+        Me.txtaltura = New System.Windows.Forms.TextBox()
+        Me.txtPeso = New System.Windows.Forms.TextBox()
+        Me.txtSintomas = New System.Windows.Forms.RichTextBox()
+        Me.BtnEnviar = New System.Windows.Forms.Button()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.BtnActualizar = New System.Windows.Forms.Button()
+        Me.BtnEliminar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ListSintomas = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TxtDescrip = New System.Windows.Forms.RichTextBox()
+        Me.TxtMedicamentosRece = New System.Windows.Forms.RichTextBox()
+        Me.txtMedicamentosAdminis = New System.Windows.Forms.RichTextBox()
+        Me.TxtDiagnostico = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.BtnEnviar)
+        Me.Panel1.Controls.Add(Me.txtSintomas)
+        Me.Panel1.Controls.Add(Me.txtPeso)
+        Me.Panel1.Controls.Add(Me.txtaltura)
+        Me.Panel1.Controls.Add(Me.txtpresion)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 38)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(397, 321)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.TxtDiagnostico)
+        Me.Panel2.Controls.Add(Me.txtMedicamentosAdminis)
+        Me.Panel2.Controls.Add(Me.TxtMedicamentosRece)
+        Me.Panel2.Controls.Add(Me.TxtDescrip)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.ListSintomas)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Location = New System.Drawing.Point(429, 38)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(426, 321)
+        Me.Panel2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Presión"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Altura"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 193)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Síntomas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 134)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Peso"
+        '
+        'txtpresion
+        '
+        Me.txtpresion.Location = New System.Drawing.Point(3, 39)
+        Me.txtpresion.Name = "txtpresion"
+        Me.txtpresion.Size = New System.Drawing.Size(100, 20)
+        Me.txtpresion.TabIndex = 4
+        '
+        'txtaltura
+        '
+        Me.txtaltura.Location = New System.Drawing.Point(3, 93)
+        Me.txtaltura.Name = "txtaltura"
+        Me.txtaltura.Size = New System.Drawing.Size(100, 20)
+        Me.txtaltura.TabIndex = 5
+        '
+        'txtPeso
+        '
+        Me.txtPeso.Location = New System.Drawing.Point(3, 161)
+        Me.txtPeso.Name = "txtPeso"
+        Me.txtPeso.Size = New System.Drawing.Size(100, 20)
+        Me.txtPeso.TabIndex = 6
+        '
+        'txtSintomas
+        '
+        Me.txtSintomas.Location = New System.Drawing.Point(3, 209)
+        Me.txtSintomas.Name = "txtSintomas"
+        Me.txtSintomas.Size = New System.Drawing.Size(257, 96)
+        Me.txtSintomas.TabIndex = 7
+        Me.txtSintomas.Text = ""
+        '
+        'BtnEnviar
+        '
+        Me.BtnEnviar.BackColor = System.Drawing.Color.Aqua
+        Me.BtnEnviar.Location = New System.Drawing.Point(286, 238)
+        Me.BtnEnviar.Name = "BtnEnviar"
+        Me.BtnEnviar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEnviar.TabIndex = 8
+        Me.BtnEnviar.Text = "Enviar"
+        Me.BtnEnviar.UseVisualStyleBackColor = False
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.BackColor = System.Drawing.Color.Aqua
+        Me.btnRegistrar.Location = New System.Drawing.Point(12, 415)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnRegistrar.TabIndex = 9
+        Me.btnRegistrar.Text = "Registrar"
+        Me.btnRegistrar.UseVisualStyleBackColor = False
+        '
+        'BtnActualizar
+        '
+        Me.BtnActualizar.BackColor = System.Drawing.Color.Aqua
+        Me.BtnActualizar.Location = New System.Drawing.Point(380, 415)
+        Me.BtnActualizar.Name = "BtnActualizar"
+        Me.BtnActualizar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnActualizar.TabIndex = 10
+        Me.BtnActualizar.Text = "Actualizar"
+        Me.BtnActualizar.UseVisualStyleBackColor = False
+        '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.BackColor = System.Drawing.Color.Aqua
+        Me.BtnEliminar.Location = New System.Drawing.Point(737, 415)
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEliminar.TabIndex = 11
+        Me.BtnEliminar.Text = "Eliminar"
+        Me.BtnEliminar.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Sintomas Mostrados"
+        '
+        'ListSintomas
+        '
+        Me.ListSintomas.FormattingEnabled = True
+        Me.ListSintomas.Location = New System.Drawing.Point(3, 31)
+        Me.ListSintomas.Name = "ListSintomas"
+        Me.ListSintomas.Size = New System.Drawing.Size(116, 82)
+        Me.ListSintomas.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(3, 116)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Descripción"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(318, 15)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(63, 13)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Diagnóstico"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(250, 116)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(144, 13)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Medicamentos Administrados"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(3, 221)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(131, 13)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Medicamentos Recetados"
+        '
+        'TxtDescrip
+        '
+        Me.TxtDescrip.Location = New System.Drawing.Point(3, 136)
+        Me.TxtDescrip.Name = "TxtDescrip"
+        Me.TxtDescrip.Size = New System.Drawing.Size(184, 70)
+        Me.TxtDescrip.TabIndex = 6
+        Me.TxtDescrip.Text = ""
+        '
+        'TxtMedicamentosRece
+        '
+        Me.TxtMedicamentosRece.Location = New System.Drawing.Point(3, 244)
+        Me.TxtMedicamentosRece.Name = "TxtMedicamentosRece"
+        Me.TxtMedicamentosRece.Size = New System.Drawing.Size(184, 70)
+        Me.TxtMedicamentosRece.TabIndex = 7
+        Me.TxtMedicamentosRece.Text = ""
+        '
+        'txtMedicamentosAdminis
+        '
+        Me.txtMedicamentosAdminis.Location = New System.Drawing.Point(230, 136)
+        Me.txtMedicamentosAdminis.Name = "txtMedicamentosAdminis"
+        Me.txtMedicamentosAdminis.Size = New System.Drawing.Size(184, 70)
+        Me.txtMedicamentosAdminis.TabIndex = 8
+        Me.txtMedicamentosAdminis.Text = ""
+        '
+        'TxtDiagnostico
+        '
+        Me.TxtDiagnostico.Location = New System.Drawing.Point(235, 39)
+        Me.TxtDiagnostico.Name = "TxtDiagnostico"
+        Me.TxtDiagnostico.Size = New System.Drawing.Size(184, 70)
+        Me.TxtDiagnostico.TabIndex = 9
+        Me.TxtDiagnostico.Text = ""
+        '
+        'FormConsultorio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(857, 450)
+        Me.Controls.Add(Me.BtnEliminar)
+        Me.Controls.Add(Me.BtnActualizar)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "FormConsultorio"
+        Me.Text = "FormConsultorio"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents BtnEnviar As Button
+    Friend WithEvents txtSintomas As RichTextBox
+    Friend WithEvents txtPeso As TextBox
+    Friend WithEvents txtaltura As TextBox
+    Friend WithEvents txtpresion As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TxtDiagnostico As RichTextBox
+    Friend WithEvents txtMedicamentosAdminis As RichTextBox
+    Friend WithEvents TxtMedicamentosRece As RichTextBox
+    Friend WithEvents TxtDescrip As RichTextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ListSintomas As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnRegistrar As Button
+    Friend WithEvents BtnActualizar As Button
+    Friend WithEvents BtnEliminar As Button
+End Class
---
+++ Proyecto.1Medico/Proyecto.1Medico/FormConsultorio.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormConsultorio
+
+End Class
