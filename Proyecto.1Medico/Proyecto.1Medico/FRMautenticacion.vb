cdf5a3be188da394b8561a73d620180c1412b0f5
WilberthMV <wilberthmartinez27@gmail.com>
2020-07-21 19:08:33 -0600

    Actualización

--- Proyecto.1Medico/Proyecto.1Medico/FRMautenticacion.vb
+++ Proyecto.1Medico/Proyecto.1Medico/FRMautenticacion.vb
@@ -1,3 +1,17 @@
 ﻿Public Class FRMautenticacion
 
+    Dim Autenticacion As New Autenticacion
+
+    Private Sub FRMautenticacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Public Sub VerificarUsuario()
+
+    End Sub
+
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles BtnIngresar.Click
+
+    End Sub
 End Class
