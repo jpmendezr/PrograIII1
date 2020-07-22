86b462d188cfc2d4db6aa884c28f54322a43c68a
MARIA HERRERA ZU�IGA <maherreraz@est.utn.ac.cr>
2020-07-21 19:18:29 -0600

    segundo comint

--- Proyecto.1Medico/Proyecto.1Medico/FRMautenticacion.vb
+++ Proyecto.1Medico/Proyecto.1Medico/FRMautenticacion.vb
@@ -1,5 +1,21 @@
 ﻿Public Class FRMautenticacion
     Private Sub FRMautenticacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+<<<<<<< HEAD
+=======
+    Dim Autenticacion As New Autenticacion
+
+    Private Sub FRMautenticacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Public Sub VerificarUsuario()
+
+    End Sub
+
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles BtnIngresar.Click
+
+>>>>>>> cdf5a3be188da394b8561a73d620180c1412b0f5
     End Sub
 End Class
