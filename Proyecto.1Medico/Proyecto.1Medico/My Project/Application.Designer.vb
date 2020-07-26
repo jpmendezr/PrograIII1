b08177969a43912847fc52e16b7db9df2ca0320c
MARIA HERRERA ZU�IGA <maherreraz@est.utn.ac.cr>
2020-07-26 16:55:39 -0600

    Nuevo Paciente

--- Proyecto.1Medico/Proyecto.1Medico/My Project/Application.Designer.vb
+++ Proyecto.1Medico/Proyecto.1Medico/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto._1Medico.FRM_Enfermeria
+            Me.MainForm = Global.Proyecto._1Medico.FRMautenticacion
         End Sub
     End Class
 End Namespace
