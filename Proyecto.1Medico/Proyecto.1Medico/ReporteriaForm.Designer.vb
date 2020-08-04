594f9d4869dc5a44640bd68ebb74031b0b413fb1
MARIA HERRERA ZU�IGA <maherreraz@est.utn.ac.cr>
2020-08-04 13:54:37 -0600

    Comin paciente

--- Proyecto.1Medico/Proyecto.1Medico/ReporteriaForm.Designer.vb
+++ Proyecto.1Medico/Proyecto.1Medico/ReporteriaForm.Designer.vb
@@ -23,12 +23,16 @@ Partial Class ReporteriaForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.FechaFin = New System.Windows.Forms.DateTimePicker()
-        Me.fechaInicio = New System.Windows.Forms.DateTimePicker()
         Me.TxtCedula = New System.Windows.Forms.TextBox()
         Me.BtnMostrar = New System.Windows.Forms.Button()
         Me.BtnBuscar = New System.Windows.Forms.Button()
@@ -40,17 +44,69 @@ Partial Class ReporteriaForm
         '
         Me.Panel1.BackColor = System.Drawing.SystemColors.ControlLight
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.ComboBox6)
+        Me.Panel1.Controls.Add(Me.ComboBox5)
+        Me.Panel1.Controls.Add(Me.ComboBox4)
+        Me.Panel1.Controls.Add(Me.ComboBox3)
+        Me.Panel1.Controls.Add(Me.ComboBox2)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
         Me.Panel1.Controls.Add(Me.DataGridView1)
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.FechaFin)
-        Me.Panel1.Controls.Add(Me.fechaInicio)
         Me.Panel1.Controls.Add(Me.TxtCedula)
         Me.Panel1.Location = New System.Drawing.Point(12, 12)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(732, 295)
         Me.Panel1.TabIndex = 0
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Location = New System.Drawing.Point(348, 44)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(41, 21)
+        Me.ComboBox6.TabIndex = 13
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(301, 44)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(41, 21)
+        Me.ComboBox5.TabIndex = 12
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(261, 43)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(34, 21)
+        Me.ComboBox4.TabIndex = 11
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(90, 44)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(43, 21)
+        Me.ComboBox3.TabIndex = 10
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(46, 44)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(38, 21)
+        Me.ComboBox2.TabIndex = 9
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 43)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(37, 21)
+        Me.ComboBox1.TabIndex = 8
         '
         'DataGridView1
         '
@@ -89,20 +145,6 @@ Partial Class ReporteriaForm
         Me.Label1.Size = New System.Drawing.Size(52, 15)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Cédula"
-        '
-        'FechaFin
-        '
-        Me.FechaFin.Location = New System.Drawing.Point(279, 44)
-        Me.FechaFin.Name = "FechaFin"
-        Me.FechaFin.Size = New System.Drawing.Size(200, 20)
-        Me.FechaFin.TabIndex = 3
-        '
-        'fechaInicio
-        '
-        Me.fechaInicio.Location = New System.Drawing.Point(15, 44)
-        Me.fechaInicio.Name = "fechaInicio"
-        Me.fechaInicio.Size = New System.Drawing.Size(200, 20)
-        Me.fechaInicio.TabIndex = 2
         '
         'TxtCedula
         '
@@ -153,9 +195,13 @@ Partial Class ReporteriaForm
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents FechaFin As DateTimePicker
-    Friend WithEvents fechaInicio As DateTimePicker
     Friend WithEvents TxtCedula As TextBox
     Friend WithEvents BtnBuscar As Button
     Friend WithEvents BtnMostrar As Button
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
