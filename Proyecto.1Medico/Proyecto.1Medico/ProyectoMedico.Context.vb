74be1832b9119b5a7167e5fee1cccc18bc785585
WilberthMV <wilberthmartinez27@gmail.com>
2020-08-26 15:12:47 -0600

    Sincronizacón

--- Proyecto.1Medico/Proyecto.1Medico/ProyectoMedico.Context.vb
+++ Proyecto.1Medico/Proyecto.1Medico/ProyectoMedico.Context.vb
@@ -27,6 +27,7 @@ Partial Public Class BdCentroMedicoEntities
     Public Overridable Property sysdiagrams() As DbSet(Of sysdiagrams)
     Public Overridable Property TbChequeoExamanes() As DbSet(Of TbChequeoExamanes)
     Public Overridable Property TbChequeoMedicamentos() As DbSet(Of TbChequeoMedicamentos)
+    Public Overridable Property TbConsultaMedica() As DbSet(Of TbConsultaMedica)
     Public Overridable Property TbEnfermedad() As DbSet(Of TbEnfermedad)
     Public Overridable Property TbEspecialidad() As DbSet(Of TbEspecialidad)
     Public Overridable Property TbFamiliarPaciente() As DbSet(Of TbFamiliarPaciente)
@@ -35,7 +36,6 @@ Partial Public Class BdCentroMedicoEntities
     Public Overridable Property TbPersonalInterno() As DbSet(Of TbPersonalInterno)
     Public Overridable Property TbPaciente() As DbSet(Of TbPaciente)
     Public Overridable Property TbCita() As DbSet(Of TbCita)
-    Public Overridable Property TbConsultaMedica() As DbSet(Of TbConsultaMedica)
 
     Public Overridable Function f_sp_BuscarPersonal(cedu As String) As ObjectResult(Of f_sp_BuscarPersonal_Result1)
         Dim ceduParameter As ObjectParameter = If(cedu IsNot Nothing, New ObjectParameter("Cedu", cedu), New ObjectParameter("Cedu", GetType(String)))
@@ -57,16 +57,6 @@ Partial Public Class BdCentroMedicoEntities
         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of MostrarPersona_Result)("MostrarPersona")
     End Function
 
-    Public Overridable Function f_Sp_Reporte_Cita(cedula As String, fechaInicio As String, fechaFinal As String) As ObjectResult(Of f_Sp_Reporte_Cita_Result)
-        Dim cedulaParameter As ObjectParameter = If(cedula IsNot Nothing, New ObjectParameter("Cedula", cedula), New ObjectParameter("Cedula", GetType(String)))
-
-        Dim fechaInicioParameter As ObjectParameter = If(fechaInicio IsNot Nothing, New ObjectParameter("FechaInicio", fechaInicio), New ObjectParameter("FechaInicio", GetType(String)))
-
-        Dim fechaFinalParameter As ObjectParameter = If(fechaFinal IsNot Nothing, New ObjectParameter("FechaFinal", fechaFinal), New ObjectParameter("FechaFinal", GetType(String)))
-
-        Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of f_Sp_Reporte_Cita_Result)("f_Sp_Reporte_Cita", cedulaParameter, fechaInicioParameter, fechaFinalParameter)
-    End Function
-
     Public Overridable Function Mostrar_ReporteriaPacientePersonal() As ObjectResult(Of Mostrar_ReporteriaPacientePersonal_Result)
         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of Mostrar_ReporteriaPacientePersonal_Result)("Mostrar_ReporteriaPacientePersonal")
     End Function
@@ -85,10 +75,20 @@ Partial Public Class BdCentroMedicoEntities
         Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of MostrarExamenes_Result)("MostrarExamenes")
     End Function
 
-    Public Overridable Function mostrar_tablas6(cedula As String) As ObjectResult(Of mostrar_tablas6_Result)
+    Public Overridable Function f_Sp_Reporte_Cita(cedula As String, fechaInicio As String, fechaFinal As String) As ObjectResult(Of f_Sp_Reporte_Cita_Result)
+        Dim cedulaParameter As ObjectParameter = If(cedula IsNot Nothing, New ObjectParameter("Cedula", cedula), New ObjectParameter("Cedula", GetType(String)))
+
+        Dim fechaInicioParameter As ObjectParameter = If(fechaInicio IsNot Nothing, New ObjectParameter("FechaInicio", fechaInicio), New ObjectParameter("FechaInicio", GetType(String)))
+
+        Dim fechaFinalParameter As ObjectParameter = If(fechaFinal IsNot Nothing, New ObjectParameter("FechaFinal", fechaFinal), New ObjectParameter("FechaFinal", GetType(String)))
+
+        Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of f_Sp_Reporte_Cita_Result)("f_Sp_Reporte_Cita", cedulaParameter, fechaInicioParameter, fechaFinalParameter)
+    End Function
+
+    Public Overridable Function mostrar_tablas(cedula As String) As ObjectResult(Of mostrar_tablas_Result)
         Dim cedulaParameter As ObjectParameter = If(cedula IsNot Nothing, New ObjectParameter("cedula", cedula), New ObjectParameter("cedula", GetType(String)))
 
-        Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of mostrar_tablas6_Result)("mostrar_tablas6", cedulaParameter)
+        Return DirectCast(Me, IObjectContextAdapter).ObjectContext.ExecuteFunction(Of mostrar_tablas_Result)("mostrar_tablas", cedulaParameter)
     End Function
 
 End Class
